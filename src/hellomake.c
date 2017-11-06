cb1140d5e64cd3463dc1233255f50ebdad58b91c
Richard <rrruales@espol.edu.ec>
2017-11-06 15:25:23 -0500

    Updated main

--- src/hellomake.c
+++ src/hellomake.c
@@ -1,8 +1,19 @@
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <hellomake.h>
 
-int main() {
-  // call a function in another file
-  myPrintHelloMake();
 
-  return(0);
+#define MAX 100
+int esPalindromo(char *palabra);
+
+int main(){
+	// call a function in another file
+	myPrintHelloMake();
+	char line[MAX];
+	printf("Input a word to check if it's a polindrome: ");
+	scanf("%s",line);
+	printf("Is palindrome? ");
+	printf((esPalindromo(line))?"true\n":"false\n");
+	return 0;
 }
